1154ab9052b6eb4b3fa4dadaf427634ab23d8fc8
Separator <is5201@mail.ru>
2018-10-05 02:27:07 +0600

    Minor changes

--- rhombs.js
+++ rhombs.js
@@ -11,13 +11,37 @@ const textures = require('./structures/textures');
 const BYTES_PER_RHOMBUS =
     defaults.BYTES_PER_DEFAULT + terrain.BYTES_PER_TERRAIN + blending.BYTES_PER_BLENDING +
     brightness.BYTES_PER_BRIGHTNESS + textures.BYTES_PER_TEXTURE;
-
+const OFFSET_DEFAULT = 0;
+const OFFSET_TERRAIN = OFFSET_DEFAULT + defaults.BYTES_PER_DEFAULT;
+const OFFSET_BLENDING = OFFSET_TERRAIN + terrain.BYTES_PER_TERRAIN;
+const OFFSET_BRIGHTNESS = OFFSET_BLENDING + blending.BYTES_PER_BLENDING;
+const OFFSET_TEXTURE = OFFSET_BRIGHTNESS + brightness.BYTES_PER_BRIGHTNESS;
 
 const generate = (size=sizes.SMALL) =>
     Buffer.alloc(size * size * BYTES_PER_RHOMBUS);
 
+const setSingleRhombus = (rhombs, offsetX, offsetY, terrainType, blendingType, brightnessLevel, brightnessType) => {
+    let mapSize = Math.sqrt(rhombs.length / BYTES_PER_RHOMBUS);
+    let offset = (offsetX + offsetY * mapSize) * BYTES_PER_RHOMBUS;
+    return rhombs.fill(
+        Buffer.concat([
+            defaults.generate(terrainType, blendingType),
+            terrain.generate(terrainType),
+            blending.generate(blendingType),
+            brightness.generate(brightnessLevel, brightnessType),
+            textures.generate(terrainType, blendingType)
+        ], BYTES_PER_RHOMBUS),
+        offset,
+        offset + BYTES_PER_RHOMBUS
+    );
+};
+
+/*const setBrightness = (rhombs, offsetX, offsetY, brightnessLevel, brightnessType) =>
+    setSingleRhombus(rhombs, offsetX, offsetY, );*/
+
 module.exports = {
-    generate
+    generate,
+    setSingleRhombus
 };
 
 /*const ENTITIES = {
--- test.js
+++ test.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
-const af = require('./airfields.js');
+const r = require('./rhombs.js');
+const terrain = require('./structures/terrain');
+const blending = require('./structures/blending');
 
-let airfields = af.generate();
-af.setTakeoffPoint(airfields, 0, 1, 0, 0);
-af.setLandingPoint(airfields, 0, 2, 0, 0);
-af.clearLandingPoint(airfields, 0);
-fs.writeFileSync('airfields', airfields);
+let rhombus = r.generate();
+
+r.setSingleRhombus(rhombus, 0, 0, terrain.SAND, blending.types.TYPE_11_11, 0, 1);
+r.setSingleRhombus(rhombus, 1, 0, terrain.SAND, blending.types.TYPE_10_10, 0, 1);
+r.setSingleRhombus(rhombus, 0, 1, terrain.SAND, blending.types.TYPE_11_00, 0, 1);
+r.setSingleRhombus(rhombus, 1, 1, terrain.SAND, blending.types.TYPE_10_00, 0, 1);
+
+fs.writeFileSync('rhombs', rhombus);
